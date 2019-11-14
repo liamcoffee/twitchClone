eb3d1d1881390776d0fb8b5424f3c752cd3a70d0
Liam Baxter <liam@coffee-design.co.uk>
2019-11-14 12:12:59 +0000

    react / redux feature complete

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,33 @@
+import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamShow = () => {
-	return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+	// because user can nivigate directly each component must fetch the streams.
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	render() {
+		if (!this.props.stream) {
+			return <div>Loading</div>;
+		}
+		return (
+			<div>
+				<h1>{this.props.stream.title}</h1>
+				<h5>{this.props.stream.description}</h5>
+			</div>
+		);
+	}
+}
+
+// second argument for this is a refrence to the exisiting props on this component
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	};
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
